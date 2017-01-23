420e775ab490cfb484c85e89de2a408b60f4d0e2
ffsommers <ssommers21@gmail.com>
2017-01-23 13:59:43 -0800

    adds conditionals in server repsonse handler to deal with different routes.

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    var obj = {
-    	 firstname: 'John',
-    	 lastname: 'Doe'
-    };
-   response.end(JSON.stringify(obj));
+   
+   if (request.url === '/') {
+   	   fs.createReadStream(__dirname +'/index.htm').pipe(response);
+   }
+
+   if (request.url === '/api') {
+   	response.writeHead(200, {'Content-Type': 'application/json'});
+   	var obj ={
+   		firstname: 'John',
+   		lastname: 'Doe'
+   	};
+   	response.end(JSON.stringify(obj));
+   }
+
     
 }).listen(1337, '127.0.0.1');
