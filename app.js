ae89a8770a989cf451efb4244ce00787d6597526
ffsommers <ssommers21@gmail.com>
2017-01-23 12:41:54 -0800

    completes response handler function.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-    
-})
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Hello world\n');
+}).listen(1337, '127.0.0.1');
