8f5e6b30bbeab866706997f885cdc726804217ab
ffsommers <ssommers21@gmail.com>
2017-01-23 12:56:31 -0800

    reformats server to handle html templates.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Hello world\n');
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    var html = fs.readFileSync(__dirname + '/index.htm');
+    response.end(html);
 }).listen(1337, '127.0.0.1');
