e0834fafdfa255a75b6842426a9477b25ec124ca
ffsommers <ssommers21@gmail.com>
2017-01-23 13:29:10 -0800

    formats server to populate html template from app.js variables by replacing html.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var fs = require('fs');
 
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    var html = fs.readFileSync(__dirname + '/index.htm');
+    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8');
+       var message = "Hello from earth...";
+    html = html.replace('{Message}', message);
     response.end(html);
+ 
 }).listen(1337, '127.0.0.1');
